038bbe861c6eb43e6f29357518a6663e5d889dc5
fearless-command-2358 <iuriseara@gmail.com>
2020-06-03 17:46:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   new = ""
   planeteer_calls.each do |element|
-    element + !
+    new << element + !
   end
 end
 
