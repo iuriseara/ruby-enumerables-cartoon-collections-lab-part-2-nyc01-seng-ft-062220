64b0e2a5dae911c0a60acaa96f6301c61b29b662
fearless-command-2358 <iuriseara@gmail.com>
2020-06-03 18:14:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   new_array = []
   while planeteer_calls.length == 4
-    puts new_array << planeteer_calls
+    puts.class
   end
 end
 
